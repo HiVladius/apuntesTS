371db98795072b838f203510a625d690a668c2e9
vladius <vladpsico@gmail.com>
2024-01-22 19:02:11 -0600

    uso de quicktype dentro de Vs

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,7 @@
 import { getPokemon } from "./genericos/get-genericos";
 
 
-getPokemon(200)
-  .then(pokemon => console.log(pokemon.sprites.front_default))
+getPokemon(151)
+  .then(pokemon => console.log(pokemon.sprites.front_shiny))
   .catch(err => console.log(err))
   .finally(() => console.log("Finalizó"));
